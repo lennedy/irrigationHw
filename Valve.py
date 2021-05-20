26c05134cf1d6df62999943376de61519cc428ae
lennedy <lennedy@gmail.com>
2021-05-19 23:30:59 -0300

    Remoção do código relacionado a execução da bridge

--- Valve.py
+++ Valve.py
@@ -1,6 +1,3 @@
-import logging
-import signal
-import random
 import time
 
 from pyhap.accessory import Accessory, Bridge
@@ -50,7 +47,7 @@ class Valve(Accessory):
         self._active = False
         
         # Add the services that this Accessory will support with add_preload_service here        
-        serv_valve             = self.add_preload_service('Valve', chars=['Active', 'InUse', 'ValveType', 'SetDuration', 'RemainingDuration', "IsConfigured", "Name"])
+        serv_valve             = self.add_preload_service('Valve', chars=['Active', 'InUse', 'ValveType', 'SetDuration', 'RemainingDuration', "Name"])
         self.char_type         = serv_valve.configure_char('ValveType', value=self._valveType)
         self.char_active       = serv_valve.configure_char('Active', value=self._state)
         self.char_inUse        = serv_valve.configure_char('InUse', value=self._inUse)
@@ -138,34 +135,3 @@ class Valve(Accessory):
         """
         print('Stopping accessory.')
 
-
-
-        
-#def get_bridge(driver):
-#    """Call this method to get a Bridge instead of a standalone accessory."""
-#    bridge = Bridge(driver, 'Bridge')
-#    temp_sensor = TemperatureSensor(driver, 'Sensor 2')
-#    temp_sensor2 = TemperatureSensor(driver, 'Sensor 1')
-#    bridge.add_accessory(temp_sensor)
-#    bridge.add_accessory(temp_sensor2)
-
-#    return bridge
-
-
-def get_accessory(driver):
-    """Call this method to get a standalone Accessory."""
-    return Valve("Valve", -1, driver, 'MyValve')
-
-
-# Start the accessory on port 51826
-driver = AccessoryDriver(port=51831)
-
-# Change `get_accessory` to `get_bridge` if you want to run a Bridge.
-driver.add_accessory(accessory=get_accessory(driver))
-
-# We want SIGTERM (terminate) to be handled by the driver itself,
-# so that it can gracefully stop the accessory, server and advertising.
-signal.signal(signal.SIGTERM, driver.signal_handler)
-
-# Start it!
-driver.start()
