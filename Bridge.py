352c4bda75d387919dc4d3367a2ab4385d352c6b
lennedy <lennedy@gmail.com>
2021-05-20 21:04:37 -0300

    Adicao da classe butao

--- Bridge.py
+++ Bridge.py
@@ -2,6 +2,7 @@ import logging
 import signal
 
 from Valve import Valve
+from MyButton import MyButton
 
 from pyhap.accessory import Accessory, Bridge
 from pyhap.accessory_driver import AccessoryDriver
@@ -12,6 +13,8 @@ from gpiozero import LED
 def get_bridge(driver):
     """Call this method to get a Bridge instead of a standalone accessory."""
     bridge = Bridge(driver, 'Bridge')
+    button1    = MyButton(5)
+    button2    = MyButton(6)
     temp_valv1 = Valve("Valve1", 16, driver, 'MyValve1')
     temp_valv2 = Valve("Valve2", 12, driver, 'MyValve2')
     led = LED(25)
