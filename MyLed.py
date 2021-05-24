169b20812eb20451a55165d1d26f95ce27402f9d
lennedy <lennedy@gmail.com>
2021-05-24 01:16:20 -0300

    Criacao da classe MyLed

---
+++ MyLed.py
@@ -0,0 +1,34 @@
+from gpiozero import LED
+from MyButton import MyDevice
+
+class SimulatedLed:
+  def __init__(self,pin):
+    self.__pin = pin
+    self.__ledState = False
+
+  def on(self):
+    self.__ledState = True
+  def off(self):
+    self.__ledState = False
+
+class MyLed(MyDevice):
+  def __init__(self, pin=-1):
+    super().__init__(pin)
+    self.__periodo = 0.25
+
+    if self.isEnable():
+      self.__led = LED(pin)
+    else:
+      self.__led = SimulatedLed(pin)
+
+  def blink(self):
+    self.__led.blink(self.__periodo,self.__periodo)
+
+  def on(self):
+    self.__led.on()
+
+  def off(self):
+    self.__led.off()
+
+
+
